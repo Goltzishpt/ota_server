2368adad1386cccfcd82ca1ee99d64e539ef0d34
Goltz <gilzagoltz@gmail.com>
2024-07-24 14:35:16 +0300

    fix[app]: deleted comment

--- run.sh
+++ run.sh
@@ -5,13 +5,10 @@ VENV="venv"
 python3.11 -m venv "${VENV}"
 
 source "${VENV}"/bin/activate
-pip show flask
 
 pip install -r requirements.txt
 
 pip install flask
-
-#export $(grep -v '^#' .env | xargs)
 
 if [ "$1" == "test" ]; then
     pytest
